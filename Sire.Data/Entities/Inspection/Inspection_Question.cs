b3c98ddd2b96bd506af0a02bd47e0c73a520f143
Debasis457 <debasis.457@gmail.com>
2022-10-08 15:32:16 +0530

    Removing nullable for NOT NULL fields

--- Sire.Data/Entities/Inspection/Inspection_Question.cs
+++ Sire.Data/Entities/Inspection/Inspection_Question.cs
@@ -10,16 +10,16 @@ namespace Sire.Data.Entities.Inspection
 {
     public class Inspection_Question :BaseEntity
     {
-        public int? inspection_id { get; set; }
+        public int inspection_id { get; set; }
 
         [ForeignKey("Question")]
         public int question_id { get; set; }
-        public int? assessor_id { get; set; }
+        public int assessor_id { get; set; }
         public User User { get; set; }
-        public int? reviewer_id { get; set; }
-        public bool? comment_by_reviewer { get; set; }
-        public bool? assesment_completed { get; set; }
-        public bool? review_completed { get; set; }
+        public int reviewer_id { get; set; }
+        public bool comment_by_reviewer { get; set; }
+        public bool assesment_completed { get; set; }
+        public bool review_completed { get; set; }
 
         
     }
